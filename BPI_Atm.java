8b3e708a74d73b46acb7a59c587000b9ae02e4a1
aven <137252774+johnraivenolazo@users.noreply.github.com>
2026-02-24 03:34:22 +0000

    feat: add input base

--- BPI_Atm.java
+++ BPI_Atm.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
+
 public class BPI_Atm {
     public static void main(String[] args) {
         ATMDispenseChain atmDispenser = new ATMDispenseChain();
-        int amount = 2970; // Example amount
-        if (amount % 100 != 0) {
-            System.out.println("Amount should be in multiples of 100s.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter amount to dispense: ");
+        int amount = scanner.nextInt();
+        if (amount % 10 != 0) {
+            System.out.println("Amount should be in multiples of 10s.");
         } else {
             atmDispenser.dispense(new Currency(amount));
         }
+        scanner.close();
     }
 }
